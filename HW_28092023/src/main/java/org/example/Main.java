1d388390fa693f8e599438bcb1058436bad9fcd0
Dmitri.Djourov <Dmitri.Djourov@gmail.com>
2023-10-01 03:15:30 +0300

    добавленно муз.сопровождение

--- HW_28092023/src/main/java/org/example/Main.java
+++ HW_28092023/src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -20,8 +19,10 @@ public class Main {
 				} while (bablo <= 0);
 
 				while (bablo > 0) {
-						System.out.println("... о...ёбть...да ты сегодня при лаве.. ну с " + bablo + " баксов ты желанный гость ... кхе... кхе...");
-						System.out.println("...ну чё ..фрайерок...не зассышь? сделаешь ставку? Рулетка ? ...или 777 ? :");
+						String videoUrl = "https://www.youtube.com/watch?v=LVR7fbkC360&ab_channel=LubaUspenskayaClub";
+						Clip.openYTV(videoUrl);
+						System.out.println("... о...ёпть...да ты сегодня при лаве.. ну с " + bablo + " баксов ты желанный гость ... кхе... кхе...");
+						System.out.println("...ну чё ...фрайерок...не зассышь? сделаешь ставку? Рулетка ? ...или 777 ? :");
 						System.out.println("1. Крутану рулеточку...");
 						System.out.println("2. Рубанусь на 777...");
 						System.out.println("3. ...та ну вас нах ... я сваливаю...\n");
@@ -36,7 +37,7 @@ public class Main {
 										//playIn777(scanner);
 										break;
 								case 3:
-										System.out.println("Оба на ...! А куда енто мы подорвались... по любому... вход рупь а вот за выход с тебя : " + bablo +" баксов... Промокашка ...тряхни-ка Буратину... кхе... кхе...");
+										System.out.println("Оба на ...! А куда енто мы подорвались... по любому... вход рупь, а вот за выход с тебя : " + bablo + " баксов... Промокашка ...тряхни-ка Буратину... кхе... кхе...");
 										return;
 								default:
 										System.out.println("Пожалуйста, выберите правильный вариант.");
@@ -44,6 +45,6 @@ public class Main {
 						}
 				}
 
-				System.out.println("Оба на ...! ...а чё...всё шоль... головка бо-бо... денежки тю-тю... во рту - кака... ну заходь еще... фрайерок... кхе... кхе!");
+				System.out.println("Оба на ...! ...а чё...всё шоль... головка бо-бо... денежки тю-тю... во рту - кака... ну заходь еще... Шлимазл... кхе... кхе!");
 		}
 }
